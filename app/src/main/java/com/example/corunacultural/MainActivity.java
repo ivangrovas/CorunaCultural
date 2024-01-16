3fb56f848328c1868b61bbf9fcc5ed32ae32a70f
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2024-01-16 10:09:13 +0100

    Detail Window Screen completed

--- app/src/main/java/com/example/corunacultural/MainActivity.java
+++ app/src/main/java/com/example/corunacultural/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.corunacultural;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
