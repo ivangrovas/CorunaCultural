2ad28f101cc73e929f86f8e4099e1eba6e069c6d
Iago <ipombof22@fpcoruna.afundacion.org>
2024-01-17 08:40:44 +0100

    Inicio de Main Screen

---
+++ app/src/main/java/com/example/corunacultural/MainSC.java
@@ -0,0 +1,14 @@
+package com.example.corunacultural;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainSC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sc);
+    }
+}
