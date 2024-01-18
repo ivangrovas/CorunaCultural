83dd5bd7d60461eb4f52306474bf600c98f2ea2f
Jhosue Arrieta <jarrietac22@fpcoruna.afundacion.org>
2024-01-18 13:56:14 +0100

    Añadido ButtonNavigationView porque al parecer al mergear se borro, ademas de colores para la botonera

--- app/src/main/java/com/example/corunacultural/ChangePasswordSC.java
+++ app/src/main/java/com/example/corunacultural/ChangePasswordSC.java
@@ -1,8 +1,15 @@
 package com.example.corunacultural;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ChangePasswordSC extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class ChangePasswordSC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password_sc);
+
+        BottomNavigationView bar = findViewById(R.id.bottomNavigation);
+        bar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.item1){
+                    Intent intent1 = new Intent(ChangePasswordSC.this,MainSC.class);
+                    startActivity(intent1);
+                }
+                if (item.getItemId() == R.id.item2){
+                    Intent intent2 = new Intent(ChangePasswordSC.this,BestRatedSC.class);
+                    startActivity(intent2);
+                }
+                if (item.getItemId() == R.id.item3){
+                    Intent intent3 = new Intent(ChangePasswordSC.this,FavouriteSC.class);
+                    startActivity(intent3);
+                }
+                if (item.getItemId() == R.id.item4){
+                    Intent intent4 = new Intent(ChangePasswordSC.this, ChangePasswordSC.class);
+                    startActivity(intent4);
+                }
+                return true;
+            }
+        });
     }
 }
