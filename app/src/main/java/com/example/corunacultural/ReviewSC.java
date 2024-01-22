cd6c3675de5a8ff101583f19750af2065b831b5f
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2024-01-22 17:39:34 +0100

    Completeda review screen

---
+++ app/src/main/java/com/example/corunacultural/ReviewSC.java
@@ -0,0 +1,37 @@
+package com.example.corunacultural;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewSC extends AppCompatActivity {
+
+    private ListAdapter listAdapter;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_sc);
+    }
+}
