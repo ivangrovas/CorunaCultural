77d7e2a22d20a3b91fbe20c26a6601dcabe77b41
Jhosue Arrieta <jarrietac22@fpcoruna.afundacion.org>
2024-02-21 17:28:43 +0100

    Añadido Toast de confirmación en el botón

--- app/src/main/java/com/example/corunacultural/ChangePasswordSC.java
+++ app/src/main/java/com/example/corunacultural/ChangePasswordSC.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +19,14 @@ public class ChangePasswordSC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password_sc);
+
+        Button changePasswordButton = findViewById(R.id.changePasswordButton);
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChangePasswordSC.this, "Contraseña actualizada", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         BottomNavigationView bar = findViewById(R.id.bottomNavigation);
         bar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -34,7 +44,6 @@ public class ChangePasswordSC extends AppCompatActivity {
                     Intent intent3 = new Intent(ChangePasswordSC.this,FavouriteSC.class);
                     startActivity(intent3);
                 }
-                
                 return true;
             }
         });
